3f899b6be299b9ac68beaa85be02bcff3c2c9919
sleepyhoon <pokjm99@naver.com>
2024-07-01 22:48:59 +0900

    [FEAT] ChatMessage 중에서 Type을 ENTER, TALK으로 하고 Join은 필요 없다고 판단하여 삭제했음.

--- src/main/java/hello/chatex/chatDto/ChatMessage.java
+++ src/main/java/hello/chatex/chatDto/ChatMessage.java
@@ -9,7 +9,9 @@ import lombok.Setter;
  * <br>date           : 2024-06-26
  * <pre>
  * <span style="color: white;">[description]</span>
- *
+ * 채팅에는 2가지 종류가 있다.
+ * - Enter : 채팅방에 처음 입장함. 그 때 안내문을 작성하기 위해 사용한다.
+ * - Talk : 채팅방에서 유저들이 작성한 채팅을 나타낸다.
  * </pre>
  * <pre>
  * <span style="color: white;">usage:</span>
@@ -29,7 +31,7 @@ import lombok.Setter;
 public class ChatMessage {
     // 메세지 타입 : 입장, 채팅
     public enum MessageType {
-        ENTER, JOIN, TALK
+        ENTER, TALK
     }
     private MessageType type; // 메세지 타입
 
