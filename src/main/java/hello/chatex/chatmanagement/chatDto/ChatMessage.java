febcce68a884283703263a37f7c816e02a9a0c54
sleepyhoon <101882530+sleepyhoon@users.noreply.github.com>
2024-07-29 20:33:33 +0900

    주석 추가

--- src/main/java/hello/chatex/chatmanagement/chatDto/ChatMessage.java
+++ src/main/java/hello/chatex/chatmanagement/chatDto/ChatMessage.java
@@ -16,6 +16,8 @@ import java.io.Serializable;
  * 채팅에는 2가지 종류가 있다.
  * - Enter : 채팅방에 처음 입장함. 그 때 안내문을 작성하기 위해 사용한다.
  * - Talk : 채팅방에서 유저들이 작성한 채팅을 나타낸다.
+ * - Join : 채팅방에 기존 유저가 입장함.
+ * - Leave : 채팅방에서 유저가 나감. (탈퇴)
  * </pre>
  * <pre>
  * <span style="color: white;">usage:</span>
@@ -32,7 +34,7 @@ import java.io.Serializable;
  */
 @Getter
 @Document(collection = "chat")
-public class ChatMessage  {
+public class ChatMessage {
     // 메세지 타입 : 새로 운 유저 입장, 기존의 유저 입장, 채팅
     public enum MessageType {
         ENTER, JOIN, TALK, LEAVE
